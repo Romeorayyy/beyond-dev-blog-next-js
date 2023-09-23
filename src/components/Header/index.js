c66566e1709a1b224ab8047b9eb76c4d9dbe5830
Randy Yono <randyyono96@gmail.com>
2023-09-23 18:29:02 -0400

    styled the navbar

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -102,13 +102,22 @@ const Header = () => {
         className=" w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize  items-center hidden sm:flex
         fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm z-50"
       >
-        <Link href="/" className="mr-2">
+        <Link
+          href="/"
+          className="mr-2 hover:scale-125 transition-all ease duration-200"
+        >
           Home
         </Link>
-        <Link href="/about" className="mx-2">
+        <Link
+          href="/about"
+          className="mx-2 hover:scale-125 transition-all ease duration-200"
+        >
           About
         </Link>
-        <Link href="/contact" className="mx-2">
+        <Link
+          href="/contact"
+          className="mx-2 hover:scale-125 transition-all ease duration-200"
+        >
           Contact
         </Link>
         <button
