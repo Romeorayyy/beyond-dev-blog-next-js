8b4b782c17bfe904ae9b4e76069da633bc46f9da
Randy Yono <randyyono96@gmail.com>
2023-09-30 04:38:20 -0400

    error handling works now

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,15 +1,24 @@
 'use client'; // The 'use client' you had is not valid, assuming you meant 'use strict'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 
 export default function ContactForm() {
   const {
     register,
     handleSubmit,
-    reset, // <-- Add this for resetting the form
+    reset,
     formState: { errors },
   } = useForm();
   const [feedback, setFeedback] = useState(null);
+
+  useEffect(() => {
+    if (feedback) {
+      const timer = setTimeout(() => {
+        setFeedback(null);
+      }, 7000);
+      return () => clearTimeout(timer);
+    }
+  }, [feedback]);
 
   const onSubmit = async (data) => {
     try {
@@ -25,7 +34,7 @@ export default function ContactForm() {
 
       if (result.success) {
         setFeedback('Email sent successfully!');
-        reset(); // <-- This will reset the form after a successful email send
+        reset();
       } else {
         setFeedback('Error sending email. Please try again later.');
       }
@@ -44,37 +53,45 @@ export default function ContactForm() {
         type="text"
         placeholder="your name"
         {...register('name', { required: true, maxLength: 80 })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-      focus:border-gray bg-transparent"
+        className={`outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b ${
+          errors.name ? 'border-red-500' : 'border-gray'
+        } 
+        focus:border-gray bg-transparent`}
       />
       and I'm reaching out regarding{' '}
       <input
         type="text"
         placeholder="e.g., a coding question, an affiliate product, financial insights"
         {...register('subject', { required: true, maxLength: 200 })}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-      focus:border-gray bg-transparent"
+        className={`outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b ${
+          errors.subject ? 'border-red-500' : 'border-gray'
+        } 
+        focus:border-gray bg-transparent`}
       />
       You can email me back at
       <input
         type="email"
         placeholder="your@email"
-        {...register('email', {})}
-        className="outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b border-gray 
-      focus:border-gray bg-transparent"
+        {...register('email', { required: true })} // Made the email field required
+        className={`outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-center placeholder:text-lg border-b ${
+          errors.email ? 'border-red-500' : 'border-gray'
+        } 
+        focus:border-gray bg-transparent`}
       />
       Here's more about my inquiry: <br />
       <textarea
         {...register('inquiryDetails', { required: true })}
         placeholder="I'd like to know more about..."
         rows={3}
-        className="w-full outline-none border-0 p-0 mx-0 focus:ring-0 placeholder:text-lg border-b border-gray 
-      focus:border-gray bg-transparent"
+        className={`w-full outline-none border-0 p-0 mx-0 focus:ring-0 placeholder:text-lg border-b ${
+          errors.inquiryDetails ? 'border-red-500' : 'border-gray'
+        } 
+        focus:border-gray bg-transparent`}
       />
       <input
         type="submit"
         value="send message"
-        className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer hover:scale-125 transition-all ease duration-200"
+        className="mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2 border-solid border-dark dark:border-light rounded cursor-pointer hover:scale-125 transition-all ease duration-200 active:scale-95"
       />
       {/* Display feedback to user */}
       {feedback && <div className="mt-4">{feedback}</div>}
