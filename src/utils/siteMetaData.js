54a68e9afa588e81d43aa25883b08bf76dc5c40e
Randy Yono <randyyono96@gmail.com>
2023-10-03 05:38:33 -0400

    .

--- src/utils/siteMetaData.js
+++ src/utils/siteMetaData.js
@@ -7,7 +7,7 @@ const siteMetadata = {
     'BeyondDevBlog by Randy explores a variety of topics including coding, finance, and affiliate marketing. The blog provides practical advice and insights, reflecting Randy’s journey and learning in technology and side hustles. Connect with a community passionate about continuous learning and diversifying interests.',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://blog-next-js-flame.vercel.app/', // your website URL
+  siteUrl: 'https://www.beyonddevblog.com', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
   email: 'randyyono96@gmail.com',
@@ -15,7 +15,7 @@ const siteMetadata = {
   twitter: 'https://twitter.com',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com/in/randyyono/',
+  linkedin: 'https://www.linkedin.com/in/randyyono',
   dribbble: 'https://www.dribbble.com',
   locale: 'en-US',
 };
