10f0837dcdf752e3ee67a478699b3f7d1f1d51ca
Randy Yono <randyyono96@gmail.com>
2023-10-03 03:17:30 -0400

    now we only have our blog also we changed the [slug.js]

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
-
-const {withContentlayer} = require("next-contentlayer")
+const { withContentlayer } = require('next-contentlayer');
 
 const nextConfig = {
-    compiler:{
-        removeConsole: true,
-    }
+  compiler: {
+    removeConsole: true,
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 };
 
 module.exports = withContentlayer({ ...nextConfig });
